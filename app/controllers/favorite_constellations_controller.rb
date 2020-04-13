9d5645d8111fa047520056faf79729d53a81e5f4
laurenyz <laurenkyu@gmail.com>
2020-04-13 16:29:47 -0400

    create favorites

--- app/controllers/favorite_constellations_controller.rb
+++ app/controllers/favorite_constellations_controller.rb
@@ -1,2 +1,13 @@
 class FavoriteConstellationsController < ApplicationController
+    def index
+        favorite_constellations = FavoriteConstellation.all
+
+        render json: favorite_constellations
+    end
+    
+    def create
+        favorite = FavoritePlanet.create(constellation_id: params["constellation_id"], user_id: params["user_id"])
+
+        render json: favorite
+    end
 end
--- app/controllers/favorite_planets_controller.rb
+++ app/controllers/favorite_planets_controller.rb
@@ -1,2 +1,13 @@
 class FavoritePlanetsController < ApplicationController
+    def index
+        favorite_planets = FavoritePlanet.all
+
+        render json: favorite_planets
+    end
+    
+    def create
+        favorite = FavoritePlanet.create(planet_id: params["planet_id"], user_id: params["user_id"])
+
+        render json: favorite
+    end
 end
