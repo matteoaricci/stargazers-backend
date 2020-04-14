2886f855b810e77df2e2f9008a6a436b22eee56c
matteoaricci <matteoaricci@DESKTOP-KID7KUE.localdomain>
2020-04-14 10:58:21 -0400

    working on backend seetup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     has_many :constellations, through: :favorite_constellations
     has_many :planets, through: :favorite_planets
     belongs_to :sign
+    has_many :conversations, :foreign_key => :sender_id
     has_secure_password
     # validates :username, uniqueness: {case_sensitive: false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,65 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_08_184059) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "constellations", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "image"
-    t.integer "sign_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "favorite_constellations", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "constellation_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "favorite_planets", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "planet_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "planets", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
-    t.text "description"
-    t.boolean "habitable"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "signs", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "image"
-    t.integer "start_month"
-    t.integer "start_day"
-    t.integer "end_month"
-    t.integer "end_day"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "username"
-    t.text "bio"
-    t.integer "birth_month"
-    t.integer "birth_day"
-    t.integer "sign_id"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
